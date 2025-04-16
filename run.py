4182481c93376027565a9e0a94ba38179f2e5a1c
TrickyInari <164391281+TrickyInari@users.noreply.github.com>
2025-04-16 08:06:18 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
-from app import create_app
+from app import create_app  # Importing the app factory function
 
+# Create Flask application instance
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)  # Remove debug=True in production
+    # Running the application on the specified IP at port 443 (not secure without SSL)
+    app.run(host='40.200.50.16', port=443, debug=True)  # For development (not secure)
